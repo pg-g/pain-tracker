cbef60679c60c20531707b23ce9edf537cbf839b
Paige <pguthrie+git@jive.com>
2015-03-12 00:51:15 -0600

    tweaks

--- js/control.js
+++ js/control.js
@@ -3,10 +3,14 @@ var app = angular.module('migraineApp');
 app.controller('mainControl', function($scope, $location){
 	$scope.timestamp = new Date();
 
+	//set logged in user
+	$scope.user = 'Paige';
+
 	$scope.addEntry = function(){
 		//add a new entry
 		
 		//log a timestamp in that entry
+		var now = new Date();
 
 		//go to log form to add more details
 		$location.path('/log');
