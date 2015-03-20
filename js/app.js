b1c3b131f53228c1ee849cb080cde3e5f9f4b40b
Paige <pguthrie+git@jive.com>
2015-03-19 21:44:42 -0600

    working

--- js/app.js
+++ js/app.js
@@ -14,3 +14,17 @@ app.config(function($routeProvider){
 			templateUrl: 'js/stats/stats.html'
 		})
 })
+
+
+// TO DO
+// map form to ind headache to update data in firebase
+// create threads to stats.html based on user
+// trick out form
+// ability to login/out
+// data visualization
+
+// d3.js
+// chart.js
+// highcharts.com
+// cytoscape.js
+// server-side charting like R or Circos
--- js/envService.js
+++ js/envService.js
@@ -12,6 +12,7 @@ app.service('envService', function($firebase, $window){
     // this.saveLog = function(now){
     // 	$window.localStorage.setItem('timestamp', now);
     // };
+    
     this.getLog = function(){
         var user = 'paige';
     	// return $window.localStorage.getItem('timestamp');
